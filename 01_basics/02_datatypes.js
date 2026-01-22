8bd3a1208240c55ecb25281e124e3717bdbb1ac3
ARTHIKMIT <arthikshetty4@gmail.com>
2026-01-22 12:55:50 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js code as newer version
+
+// alert("3+3")  // we are using nodejs, not browser   
+
+console.log("arthik");
+
+let name = "arthik"
+let age = 18 
+let isloggedIn = false
+
+// number => 2 to power 53
+// bigint  // it is used while using a large numbers
+//  string => ""
+//  boolean => true/false
+// null => standalone value
+//  undefined=> //intetionally not valued
+//  symbol  => uniqueness 
+ 
+console.log(typeof null) // null is a type of object 
+console.log(typeof undefined) // undefined is a type of undefined itself 
